802efb8c31389083b5862471e7158e1f2195d616
Hans Schülein <contact.kamik423@gmail.com>
2025-12-02 01:50:54 +0100

    Scalaisms

--- 01.scala
+++ 01.scala
@@ -1,14 +1,12 @@
 import Math.{floorMod, floorDiv}
 
 // Add .zeros to Iterable of Integers
-implicit class ZeroCountingIntIterable(items: Iterable[Int]) {
+implicit class ZeroCountingIntIterable(items: Iterable[Int]):
     def zeros: Int = items.count(_ % 100 == 0)
-}
 
 // Add .last to Sequences
-implicit class EndingIterable[T](items: Seq[T]) {
+implicit class EndingIterable[T](items: Seq[T]):
     def last: T = items(items.length - 1)
-}
 
 // Data structure that contains a result and an execution time
 case class TimedExecution[T](result: T, nanoSeconds: Double):
@@ -16,31 +14,28 @@ case class TimedExecution[T](result: T, nanoSeconds: Double):
     def milliseconds = nanoSeconds / 1_000_000.0
     def seconds = nanoSeconds / 1_000_000_000.0
     // Pretty string format duration
-    def prettyDuration: String = this match {
+    def prettyDuration: String = this match
         case _ if nanoSeconds < 100.0  => f"$nanoSeconds%.2f ns"
         case _ if microseconds < 100.0 => f"$microseconds%.2f µs"
         case _ if milliseconds < 100.0 => f"$milliseconds%.2f ms"
         case _                         => f"$seconds%.2f s"
-    }
 
     override def toString: String =
         if result.isInstanceOf[Int] then f"$prettyDuration ==> $result"
         else prettyDuration
 
 object TimedExecution:
-    def timing[T](block: => T): TimedExecution[T] = {
+    def timing[T](block: => T): TimedExecution[T] =
         val before = System.nanoTime
         val result = block
         val after = System.nanoTime
-        return TimedExecution(result, after - before)
-    }
+        TimedExecution(result, after - before)
 
 // Time a block and print a message
-def time[T](message: String)(block: => T): T = {
+def time[T](message: String)(block: => T): T =
     val result = TimedExecution.timing(block)
-    println(f"$message ${result}")
+    println(f"$message $result")
     result.result
-}
 
 def wrap = (number: Int) => floorMod(number, 100)
 
